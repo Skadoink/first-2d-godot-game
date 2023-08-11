830e2ae013474fefb8988576118e64d9eb2b8f5f
Skadoink <oskaege@gmail.com>
2023-08-12 09:54:08 +1200

    start button bind

--- Main.gd
+++ Main.gd
@@ -26,7 +26,7 @@ func game_over():
 func new_game():
 	score = 0
 	print(difficulty)
-	$MobTimer.set_wait_time(0.5 / (difficulty / 3))
+	$MobTimer.set_wait_time(0.25 / (difficulty / 3))
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
